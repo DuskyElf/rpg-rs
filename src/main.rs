d46ac2f35488416aec1150ef83e4554441635586
DuskyElf <clayedison75@gmail.com>
2022-09-25 12:18:52 +0530

    Question handling & State handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use pancurses::*;
-use std::{thread, time};
+use std::{thread, time, fmt::Write};
 
 mod models;
 use models::*;
@@ -10,10 +10,11 @@ fn main() {
     let mut game = Game::new(initscr(), vec![
         Message::INFO("Hello, World! This is just so awesome!!!".to_string()),
         Message::QUESTION("What is your name?".to_string(), 0),
+        Message::QUESTION("What is your age?".to_string(), 1),
+        Message::INFO("Hi $0, and you are $1 years old!".to_string()),
     ]);
 
     start_game(&mut game);
-    game.window.getch();
 
     endwin();
 }
@@ -48,19 +49,91 @@ fn question_message(question: &String, id: &usize, game: &mut Game) {
     tell_info(question, game);
 
     game.window.addstr("\n\n>");
-    let responce = String::new();
-    // scanf(&game.window, &mut responce);
+    let mut responce = String::new();
+    scan(&game.window, &mut responce);
 
-    game.window.addstr(&responce);
+    game.states.insert(*id, responce);
 }
 
 fn tell_info(info: &String, game: &Game) {
     game.window.clear();
     game.window.mv(0, 0);
-    for char in info.chars() {
-        game.window.addch(char as u32);
+
+    let viewable = parse(info, game);
+
+    for letter in viewable.chars() {
+        game.window.addch(letter as u32);
         thread::sleep(time::Duration::from_millis(15));
         game.window.refresh();
+    }
+}
+
+fn parse(info: &String, game: &Game) -> String {
+    let mut i = 0;
+    let mut result = String::new();
+    while i < info.len() {
+        let letter = info.chars().nth(i).unwrap();
+        if letter == '$' {
+            result += &handel_states(&mut i, info, game);
+        }
+        else {
+            result.write_char(letter).unwrap();
+            i += 1;
+        }
+    }
+    result
+}
+
+fn handel_states(i: &mut usize, info: &String, game: &Game) -> String {
+    *i += 1;
+    let mut result = String::new();
+    if *i < info.len() {
+        let mut letter = info.chars().nth(*i).unwrap();
+        let mut number = String::new();
+        while ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].contains(&letter) {
+            number.write_char(letter).unwrap();
+            *i += 1;
+            if *i >= info.len() { break; }
+            letter = info.chars().nth(*i).unwrap();
+        }
+        if !number.is_empty() {
+            let number: usize = number.parse().unwrap();
+            result += &game.states[&number];
+        }
+        else {
+            result.write_char(info.chars().nth(*i-1).unwrap()).unwrap();
+        }
+
+        if *i >= info.len() { return result; }
+    }
+    else {
+        result.write_char(info.chars().nth(*i-1).unwrap()).unwrap();
+    }
+    result
+}
+
+fn scan(window: &Window, buffer: &mut String) {
+    noecho();
+    loop {
+        if let Input::Character(read) = window.getch().unwrap() {
+            if read == '\n' {
+                echo();
+                break;
+            }
+
+            // backspace
+            if read == '\x7f' {
+                if buffer.len() != 0 {
+                    buffer.pop();
+                    window.mv(window.get_cur_y(), window.get_cur_x() - 1);
+                    window.delch();
+                }
+                continue;
+            }
+
+            window.addch(read);
+            buffer.push(read);
+        }
     }
 }
 
